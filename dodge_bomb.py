3fa7640638b94bea563d5a5c952197994dbc86d7
Kosuke Takahasi <c0a2413915@edu.teu.ac.jp>
2025-09-30 14:37:31 +0900

    define dodge_bomb

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,20 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数はこうかとんRect or 爆弾Rect
+    戻り値:判定結果タプル（横方向, 縦方向）
+    画面内ならTrue/画面がいならFalse
+    """
+    yoko = True
+    tate = True
+    if rct.left < 0 or WIDTH < rct.right:  #横方向にはみ出ていたら
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  #縦方向にはみ出していたら
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -50,8 +64,15 @@ def main():
         #if key_lst[pg.K_RIGHT]:
             #sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
